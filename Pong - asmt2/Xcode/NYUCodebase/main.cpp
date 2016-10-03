b46b02710ae7247d2ac2e50eb8bab461fff9be44
Brian Quinn <bjq207@nyu.edu>
2016-10-03 13:56:12 -0400

    Preliminary Edits

--- Pong - asmt2/Xcode/NYUCodebase/main.cpp
+++ Pong - asmt2/Xcode/NYUCodebase/main.cpp
@@ -6,6 +6,7 @@
 #include <SDL_image.h>
 #include "ShaderProgram.h"
 #include "Matrix.h"
+#include <random>
 
 #ifdef _WINDOWS
 #define RESOURCE_FOLDER ""
@@ -32,6 +33,21 @@ GLuint LoadTexture(const char* image_path) {
     return textureID;
 }
 
+struct Paddle
+{
+    Paddle(): x(0), y(0) {}
+    float x;
+    float y;
+};
+
+struct Ball
+{
+    Ball(): x(0), y(0) {}
+    float x;
+    float y;
+    float arr[2] = {1.5, 1.5};
+};
+
 int main(int argc, char *argv[])
 {
     SDL_Init(SDL_INIT_VIDEO);
@@ -49,29 +65,41 @@ int main(int argc, char *argv[])
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
     
-    ShaderProgram program(RESOURCE_FOLDER "vertex_textured.glsl", RESOURCE_FOLDER "fragment_textured.glsl");
-    
-    GLuint redChip = LoadTexture("/Images/chipRedWhite_sideBorder.png");
-    GLuint greenChip = LoadTexture("/Images/chipGreenWhite_sideBorder.png");
-    GLuint blueChip = LoadTexture("/Images/chipBlueWhite_sideBorder.png");
-    GLuint blackChip = LoadTexture("/Images/chipBlackWhite_sideBorder.png");
+    ShaderProgram program(RESOURCE_FOLDER "vertex.glsl", RESOURCE_FOLDER "fragment.glsl");
     
     Matrix projectionMatrix;
-    Matrix modelMatrix;
     Matrix viewMatrix;
     
-    float dropchip1 = 2.0;
-    float dropchip2 = 3.0;
+    Matrix paddleMatrix;
+    Matrix paddleMatrix2;
+    Matrix ballMatrix;
+    
+    Paddle left;
+    Paddle right;
+    Ball ball;
+    
+    float paddleVertices[] = {
+        -0.1, -0.5, // Triangle 1 Coord A
+        0.1, -0.5,  // Triangle 1 Coord B
+        0.1, 0.5,   // Triangle 1 Coord C
+        -0.1, -0.5, // Triangle 2 Coord A
+        0.1, 0.5,   // Triangle 2 Coord B
+        -0.1, 0.5   // Triangle 2 Coord C
+    };
+    
+    float ballVertices[] = {
+        -0.1, -0.1, // Triangle 1 Coord A
+        0.1, -0.1,  // Triangle 1 Coord B
+        0.1, 0.1,   // Triangle 1 Coord C
+        -0.1, -0.1, // Triangle 2 Coord A
+        0.1, 0.1,   // Triangle 2 Coord B
+        -0.1, 0.1   // Triangle 2 Coord C
+    };
     
     projectionMatrix.setOrthoProjection(-7.0f, 7.0f, -4.0f, 4.0f, -1.0f, 1.0f);
     glUseProgram(program.programID);
     
     while (!done) {
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT || event.type == SDL_WINDOWEVENT_CLOSE) {
-                done = true;
-            }
-        }
         
         glClear(GL_COLOR_BUFFER_BIT);
         
@@ -79,119 +107,62 @@ int main(int argc, char *argv[])
         float elapsed = ticks - lastFrameTicks;
         lastFrameTicks = ticks;
         
-        program.setModelMatrix(modelMatrix);
         program.setProjectionMatrix(projectionMatrix);
         program.setViewMatrix(viewMatrix);
         
-        //            float vertices[] = {0.5f, -0.5f, 0.0f, 0.5f, -0.5f, -0.5f};
-        //            glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, vertices);
-        //            glEnableVertexAttribArray(program.positionAttribute);
-        //            glDrawArrays(GL_TRIANGLES, 0, 3);
-        //            glDisableVertexAttribArray(program.positionAttribute);
-        
-        float vertices[] = {
-            // First Square
-            -0.7, -0.5, // Triangle 1 Coord A
-            0.7, -0.5,  // Triangle 1 Coord B
-            0.7, 0.5,   // Triangle 1 Coord C
-            -0.7, -0.5, // Triangle 2 Coord A
-            0.7, 0.5,   // Triangle 2 Coord B
-            -0.7, 0.5   // Triangle 2 Coord C
-        };
-        glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, vertices);
-        glEnableVertexAttribArray(program.positionAttribute);
-        
-        glBindTexture(GL_TEXTURE_2D, redChip);
-        float texCoords[] = {
-            0.0, 1.0,
-            1.0, 1.0,
-            1.0, 0.0,
-            0.0, 1.0,
-            1.0, 0.0,
-            0.0, 0.0
-        };
-        glVertexAttribPointer(program.texCoordAttribute, 2, GL_FLOAT, false, 0, texCoords);
-        glEnableVertexAttribArray(program.texCoordAttribute);
-        glDrawArrays(GL_TRIANGLES, 0, 6);
-        
-        float vertices1[] = {
-            -0.7, -0.25,
-            0.7, -0.25,
-            0.7, 0.75,
-            -0.7, -0.25,
-            0.7, 0.75,
-            -0.7, 0.75
-        };
-        glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, vertices1);
-        glEnableVertexAttribArray(program.positionAttribute);
-        
-        glBindTexture(GL_TEXTURE_2D, greenChip);
-        float texCoords1[] = {
-            0.0, 1.0,
-            1.0, 1.0,
-            1.0, 0.0,
-            0.0, 1.0,
-            1.0, 0.0,
-            0.0, 0.0
-        };
-        glVertexAttribPointer(program.texCoordAttribute, 2, GL_FLOAT, false, 0, texCoords1);
-        glEnableVertexAttribArray(program.texCoordAttribute);
-        glDrawArrays(GL_TRIANGLES, 0, 6);
-        
-        float vertices2[] = {
-            -0.7, 0.0,
-            0.7, 0.0,
-            0.7, 1.0,
-            -0.7, 0.0,
-            0.7, 1.0,
-            -0.7, 1.0
-        };
-        glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, vertices2);
-        glEnableVertexAttribArray(program.positionAttribute);
-        
-        glBindTexture(GL_TEXTURE_2D, blueChip);
-        float texCoords2[] = {
-            0.0, 1.0,
-            1.0, 1.0,
-            1.0, 0.0,
-            0.0, 1.0,
-            1.0, 0.0,
-            0.0, 0.0
-        };
-        glVertexAttribPointer(program.texCoordAttribute, 2, GL_FLOAT, false, 0, texCoords2);
-        glEnableVertexAttribArray(program.texCoordAttribute);
-        glDrawArrays(GL_TRIANGLES, 0, 6);
-        
-        float vertices3[] = {
-            -0.7, dropchip1,
-            0.7, dropchip1,
-            0.7, dropchip2,
-            -0.7, dropchip1,
-            0.7, dropchip2,
-            -0.7, dropchip2
-        };
-        glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, vertices3);
-        glEnableVertexAttribArray(program.positionAttribute);
-        
-        if (dropchip1 > 0.25) {
-            dropchip1 -= elapsed * 0.5;   //0.01;
-            dropchip2 -= elapsed * 0.5;   //0.01;
-        } else {
-            dropchip1 = 2.0;
-            dropchip2 = 3.0;
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT || event.type == SDL_WINDOWEVENT_CLOSE) {
+                done = true;
+            }
+            if (event.type == SDL_KEYDOWN) {
+                if (event.key.keysym.scancode == SDL_SCANCODE_UP) {
+                    right.y += 15.0 * elapsed;
+                }
+            }
+            if (event.type == SDL_KEYDOWN) {
+                if (event.key.keysym.scancode == SDL_SCANCODE_DOWN) {
+                    right.y -= 15.0 * elapsed;
+                }
+            }
+            if (event.type == SDL_KEYDOWN) {
+                if (event.key.keysym.scancode == SDL_SCANCODE_W) {
+                    left.y += 15.0 * elapsed;
+                }
+            }
+            if (event.type == SDL_KEYDOWN) {
+                if (event.key.keysym.scancode == SDL_SCANCODE_S) {
+                    left.y -= 15.0 * elapsed;
+                }
+            }
         }
-        glBindTexture(GL_TEXTURE_2D, blackChip);
-        float texCoords3[] = {
-            0.0, 1.0,
-            1.0, 1.0,
-            1.0, 0.0,
-            0.0, 1.0,
-            1.0, 0.0,
-            0.0, 0.0
-        };
-        glVertexAttribPointer(program.texCoordAttribute, 2, GL_FLOAT, false, 0, texCoords3);
-        glEnableVertexAttribArray(program.texCoordAttribute);
+        
+        if (ball.y >= 3.895 || ball.y <= -3.895) {
+            ball.arr[1] = -ball.arr[1];
+        }
+        
+        ball.x += ball.arr[0] * elapsed;
+        ball.y += ball.arr[1] * elapsed;
+        
+        program.setModelMatrix(paddleMatrix);
+        glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, paddleVertices);
+        glEnableVertexAttribArray(program.positionAttribute);
+        paddleMatrix.identity();
         glDrawArrays(GL_TRIANGLES, 0, 6);
+        paddleMatrix.Translate(-6.9, left.y, 0);
+        
+        program.setModelMatrix(paddleMatrix2);
+        glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, paddleVertices);
+        glEnableVertexAttribArray(program.positionAttribute);
+        paddleMatrix2.identity();
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+        paddleMatrix2.Translate(6.9, right.y, 0);
+        
+        program.setModelMatrix(ballMatrix);
+        glVertexAttribPointer(program.positionAttribute, 2, GL_FLOAT, false, 0, ballVertices);
+        glEnableVertexAttribArray(program.positionAttribute);
+        ballMatrix.identity();
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+        ballMatrix.Translate(ball.x, ball.y, 0);
         
         glDisableVertexAttribArray(program.positionAttribute);
         glDisableVertexAttribArray(program.texCoordAttribute);
